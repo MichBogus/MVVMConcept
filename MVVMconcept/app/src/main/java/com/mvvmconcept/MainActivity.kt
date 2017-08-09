b50af1c62d5054fb9cac9bdf9df7be630ac92cbd
Michal Bogus <m@m.com>
2017-08-09 14:03:33 +0200

    MVVM-T-1 - Added first controller and model implementation.

--- MVVMconcept/app/src/main/java/com/mvvmconcept/MainActivity.kt
+++ MVVMconcept/app/src/main/java/com/mvvmconcept/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.mvvmconcept
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import com.mvvmconcept.base.BaseActivity
+import com.mvvmconcept.databinding.ActivityMainBinding
+import com.mvvmconcept.mvvm.mainactivity.WeatherModel
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.weather = WeatherModel().apply {
+            location = "a"
+            locationTemperature = "20"
+        }
     }
 }
